64ce4a7fe5bf063fb7ede43287c73edff68dca2e
BullBot <dbullaustintx@gmail.com>
2021-06-17 05:54:41 -0500

    set search box in HTML, Bootstrap loaded

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,11 @@ console.log(button);
 function myFunction() {
   console.log("Hello");
 }
+//  Method for using "date" objects obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+const currentDate = new Date(response.data.dt * 1000);
+console.log(currentDate);
+const day = currentDate.getDate();
+const month = currentDate.getMonth() + 1;
+const year = currentDate.getFullYear();
+nameEl.innerHTML =
+  response.data.name + " (" + month + "/" + day + "/" + year + ") ";
