021f886b1e6de2d2cc23f4c59c7771be2754a470
BullBot <dbullaustintx@gmail.com>
2021-06-19 04:45:52 -0500

    created html cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,15 @@
-var button = document
-  .getElementById("searchBtn")
-  .addEventListener("click", myFunction);
-console.log(button);
+let searchBtn = document.querySelector("#search-btn")
+let citySearch = document.querySelector("#search-box")
+  searchBtn.addEventListener("click", ()=>{
+    console.log(button);
+  });
+
+
+
+
 function myFunction() {
   console.log("Hello");
-}
-//  Method for using "date" objects obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-const currentDate = new Date(response.data.dt * 1000);
-console.log(currentDate);
-const day = currentDate.getDate();
-const month = currentDate.getMonth() + 1;
-const year = currentDate.getFullYear();
-nameEl.innerHTML =
-  response.data.name + " (" + month + "/" + day + "/" + year + ") ";
-
-  api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
+  // api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
   fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=san%20francisco%2Cus&units=imperial&lang=english", {
     "method": "GET",
     "headers": {
@@ -28,3 +23,16 @@ nameEl.innerHTML =
   .catch(err => {
     console.error(err);
   });
+}
+
+//  Method for using "date" objects obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
+// const currentDate = new Date(response.data.dt * 1000);
+// console.log(currentDate);
+// const day = currentDate.getDate();
+// const month = currentDate.getMonth() + 1;
+// const year = currentDate.getFullYear();
+// nameEl.innerHTML =
+//   response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+
+
+  
