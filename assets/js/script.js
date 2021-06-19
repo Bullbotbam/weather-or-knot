147b7a387732ab7ac0d6bd97412ee34c90f2f0a3
BullBot <dbullaustintx@gmail.com>
2021-06-19 04:08:38 -0500

    created call for api

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,18 @@ const month = currentDate.getMonth() + 1;
 const year = currentDate.getFullYear();
 nameEl.innerHTML =
   response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+
+  api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid={API key}
+  fetch("https://community-open-weather-map.p.rapidapi.com/forecast?q=san%20francisco%2Cus&units=imperial&lang=english", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-key": "5c7de9724dmsh5a75b580091d59bp1083f5jsn1a89bc56379b",
+      "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
+    }
+  })
+  .then(response => {
+    console.log(response);
+  })
+  .catch(err => {
+    console.error(err);
+  });
