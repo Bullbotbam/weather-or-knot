7aaf981d1246050b44a6f510c59955fa592d98ef
BullBot <dbullaustintx@gmail.com>
2021-06-20 23:52:20 -0500

    added element from js to show weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,6 @@
 let searchBtn = document.querySelector("#search-btn")
 let citySearch = document.querySelector("#search-box")
-  searchBtn.addEventListener("click", ()=>{
-    console.log("button");
-    weatherApp()
-  });
+
 
 
 
@@ -12,7 +9,7 @@ let citySearch = document.querySelector("#search-box")
 
 async function weatherApp  () {
   // openweathermap url endpoint  
-const URL = "https:/api.openweathermap.org/data/2.5/forecast?q="
+const URL = "https:/api.openweathermap.org/data/2.5/forecast?q=" + "&units=imperial"
 
 // api key 
 const API_KEY = "&appid=2cbbc321cf75eab66be5e594226b3a84"
@@ -25,19 +22,37 @@ const API_KEY = "&appid=2cbbc321cf75eab66be5e594226b3a84"
   let data = await response.json()
   console.log(data)
   function useApiData (data){
-    // assign the data to be used to the container
-    document.querySelector("#search-display").innerHTML = `<div class="card col-8 offset-2">
-    <div class="card">
-    <div class="card-body">
-    <h4 class="city-name">${data.city.name}</h4>  
-      This is some text within a card body.
-    </div>
-  </div>`
-  
+    
+ 
+
   }
+  // assign the data to be used to the container
+  document.querySelector("#search-display").innerHTML = `<div class="card col-8 offset-2">
+  <div class="card">
+  <div class="card-body">
+  <h4 class="city-name"><strong>${data.city.name}</strong></h4>  
+  <h3 class="search-date"><strong>${data.list.dt}</strong></h3>  
+  <p class="search-temp">${data.list.main.temp}</p>
+  <p class="search-wind">${data.list}</p>
+  <p class="search-humidity">${data.city.main.humidity}</p>
+  <p class="search-uv">${}</p>
+  </div>
+</div>`
+  let weatherNow = `${data.city.name}`;
+  console.log(weatherNow)
   console.log(useApiData)
   console.log(data)
+
+//  const currentDate = new Date(response.data.dt * 1000);
+// console.log(currentDate);
+// const day = currentDate.getDate();
+// const month = currentDate.getMonth() + 1;
+// const year = currentDate.getFullYear();
+// nameEl.innerHTML =
+//   response.data.name + " (" + month + "/" + day + "/" + year + ") ";
 }
+
+
 
 
 
@@ -50,11 +65,8 @@ const API_KEY = "&appid=2cbbc321cf75eab66be5e594226b3a84"
   // });
 
 
-//  Method for using "date" objects obtained from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-// const currentDate = new Date(response.data.dt * 1000);
-// console.log(currentDate);
-// const day = currentDate.getDate();
-// const month = currentDate.getMonth() + 1;
-// const year = currentDate.getFullYear();
-// nameEl.innerHTML =
-//   response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+
+searchBtn.addEventListener("click", ()=>{
+  console.log("button");
+  weatherApp()
+});
